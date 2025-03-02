aedbebe48e6e2b4b72a6a60ce7320636214c3dea
minhbao1705 <vo.minhbao9406@gmail.com>
2025-03-02 20:18:24 +0700

    lc27

---
+++ LeetCode/Leet_Code#27-Remove_Element.py
@@ -0,0 +1,15 @@
+from typing import List
+    
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+        return j
+    
+nums = [0,1,2,2,3,0,4,2]
+val = 2
+solution = Solution()
+print(solution.removeElement(nums, val))
