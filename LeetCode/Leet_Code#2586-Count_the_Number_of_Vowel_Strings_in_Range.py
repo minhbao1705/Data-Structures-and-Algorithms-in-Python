101cae513e38d4e3e7be97c866702b0126eaf60a
minhbao1705 <vo.minhbao9406@gmail.com>
2025-03-06 07:02:47 +0700

    lc2586

--- LeetCode/Leet_Code#2586-Count_the_Number_of_Vowel_Strings_in_Range.py
+++ LeetCode/Leet_Code#2586-Count_the_Number_of_Vowel_Strings_in_Range.py
@@ -1,15 +1,16 @@
 from typing import List
 
-words = ["are","amy","u"]
 class Solution:
     def vowelStrings(self, words: List[str], left: int, right: int) -> int:
         vowel = ['a', 'i', 'u', 'e', 'o']
         out = 0
         for i in words[left:right+1]:
-                if (i[0] in vowel) and (i[-1] in vowel):
-                    out += 1
+            if (i[0] in vowel) and (i[-1] in vowel):
+                out += 1
         return out
+    
 
+words = ["are","amy","u"]
 left = 0
 right = 2
 solution = Solution()
